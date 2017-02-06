3bcf34530bcfa4d54c221c2aa78e24f42380a488
71619997a <gmarks@stuy.edu>
2017-02-05 22:32:59 -0500

    made courses easier to read, started on students

--- db_builder.py
+++ db_builder.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import csv
 
-server = MongoClient('149.89.150.100')
+server = MongoClient('127.0.0.1')  # '149.89.150.100')
 db = server.bortfielddb
 collection = db.students
 
@@ -9,8 +9,23 @@ sf = open('peeps.csv')
 cf = open('courses.csv')
 sr = csv.reader(sf)
 cr = csv.reader(cf)
-cr.next()
+cr.next()  # skip column descriptions
+# we want a list of courses, [[id, {name: x, mark: y}], ...]
+# this will make it easier to pick a student's courses
+courses = []
+for course in cr:
+    entry = [int(course[2])]  # id
+    cinfo = {'name': course[0], 'mark': course[1]}
+    entry.append(cinfo)
+    courses.append(entry)
+
+print courses
+
 sr.next()
 for student in sr:
     print student
-
+    id = int(student[2])
+    age = int(student[1])
+    name = int(student[0])
+    myCourses = filter(lambda course: id == course[0], courses)
+    # the filter gets every course of the student
